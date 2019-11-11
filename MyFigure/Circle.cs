afee8ec2e725ea15e9c381528d5c1dd02bf90a18
AlexZakal <zakalizhnuy-95@yandex.ru>
2019-11-11 13:59:46 +0300

    Добавьте файлы проекта.

---
+++ MyFigure/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace MyFigure
+{
+    public class Circle
+    {
+        public static void Squre(double Radius)
+        {
+            var SQR = 2 * (Math.PI) * Radius;
+            Console.WriteLine($"Площадь круга с радиусом {Radius} равна {SQR}");
+        }
+    }
+
+}
---
+++ MyFigure/Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace MyFigure
+{
+    public class Triangle
+    {
+        public static void Square(double A, double B, double C)
+        {
+            var P = (A + B + C)*0.5;
+            var SQR = Math.Sqrt(P * (P - A) * (P - B) * (P - C));
+            Console.WriteLine($"Площадь треугольника со сторонами A = {A}, B = {B}, C = {C} равна {SQR}");
+        }
+
+        public static bool IsRightTriangle(double A, double B, double C)
+        {
+            if ((Math.Pow(A, 2) == Math.Pow(B, 2) + Math.Pow(C, 2)) || (Math.Pow(B, 2) == Math.Pow(A, 2) + Math.Pow(C, 2)) || (Math.Pow(C, 2) == Math.Pow(A, 2) + Math.Pow(B, 2)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
